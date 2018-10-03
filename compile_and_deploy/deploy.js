53484adf586eb55674c8d5391f771deaf3c26b6f
abhilashreddyy <abhilashreddyy1@gmail.com>
2018-10-03 15:40:17 +0530

    Update deploy.js

--- compile_and_deploy/deploy.js
+++ compile_and_deploy/deploy.js
@@ -1,13 +1,8 @@
 var fs = require('fs');
 let Web3 = require('web3');
 let web3 = new Web3();
- // https://www.npmjs.com/package/web3
 
-// Create a web3 connection to a running geth node over JSON-RPC running at
-// http://localhost:8545
-// For geth VPS server + SSH tunneling see
-// https://gist.github.com/miohtama/ce612b35415e74268ff243af645048f4
-
+// set the local provider of block chain
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8080'));
 
 
@@ -22,7 +17,7 @@ let SampleContract = web3.eth.contract(abi);
 
 // Unlock the coinbase account to make transactions out of it
 console.log("Unlocking coinbase account");
-var password = "1220047c3c19";
+var password = "***********"; // replace * with your password set while creating account
 console.log("\naccount : ","\n\n ");
 var current_acc = web3.eth.accounts[0]
 try {
@@ -36,10 +31,6 @@ try {
 console.log("Deploying the contract");
 let contract = SampleContract.new({from: current_acc, gas: 1000000, data: code});
 
-// Transaction has entered to geth memory pool
-console.log("Your contract is being deployed in transaction at http://testnet.etherscan.io/tx/" + contract.transactionHash);
-
-// http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -50,8 +41,7 @@ async function waitBlock() {
   while (true) {
     let receipt = web3.eth.getTransactionReceipt(contract.transactionHash);
     if (receipt && receipt.contractAddress) {
-      console.log("Your contract has been deployed at http://testnet.etherscan.io/address/" + receipt.contractAddress);
-      console.log("Note that it might take 30 - 90 sceonds for the block to propagate befor it's visible in etherscan.io");
+      console.log("Your contract has been deployed at : " + receipt.contractAddress);
       break;
     }
     console.log("Waiting a mined block to include your contract... currently in block " + web3.eth.blockNumber);
