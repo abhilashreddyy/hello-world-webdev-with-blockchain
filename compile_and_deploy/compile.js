b84dec2e6c67b80efaaa82ffee623b85cff8ddab
abhilashreddyy <abhilashreddyy1@gmail.com>
2018-10-03 15:39:38 +0530

    Update compile.js

--- compile_and_deploy/compile.js
+++ compile_and_deploy/compile.js
@@ -1,3 +1,4 @@
+// import required modules
 var solc = require('solc');
 var fs = require('fs');
 let Web3 = require('web3');
@@ -6,13 +7,12 @@ let web3 = new Web3();
 var inputFilePath = process.argv[2];
 var outputPath = process.argv[3];
 
+
 var contractSolidity = fs.readFileSync(inputFilePath , 'utf-8');
 if (!contractSolidity)
     return console.error('unable to read file: ' + inputFilePath );
 
 var output = solc.compile(contractSolidity, 1);
-//console.log("out : ",output);
-//console.log("cont : ",  output.contracts);
 
 var temp_abi = output.contracts[":helloWorld"].interface;
 var temp_code = output.contracts[":helloWorld"].bytecode;
